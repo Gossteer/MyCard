f7fa06debe87faf01f6f30d01e9a992f6311a020
Gossteer <GossteerPlus@gmai.com>
2020-10-04 16:20:20 +0300

    add cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,11 @@
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <x-jet-welcome />
             </div>
+
+
+
         </div>
     </div>
+
+
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
+        @stack('style')
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         @livewireStyles
