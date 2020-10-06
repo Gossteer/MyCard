04db27432328059d78fc6e0158a40343204e566f
Игнатьев Антон Александрович <ignatev.a@niip.ru>
2020-10-06 16:53:42 +0300

    add_js_delay

--- app/Http/Livewire/Cards.php
+++ app/Http/Livewire/Cards.php
@@ -8,12 +8,19 @@ class Cards extends Component
 {
     public $component_edit_text = 'cards.meinTextCards';
     public $text= 'Чебурек не человек, человек не чебурек';
+    public $textt = 'lol1';
     public $linkHiden = true;
 
     public function component_edit_text_cheng()
     {
         $this->linkHiden = true;
         $this->component_edit_text = 'cards.meinTextCardsCheng';
+    }
+
+    public function lol()
+    {
+        $this->textt = 'pamagite';
+
     }
 
     public function render()
--- resources/views/livewire/cards.blade.php
+++ resources/views/livewire/cards.blade.php
@@ -1,15 +1,15 @@
     <li class="articles__article" style="--animation-order:1">
+        <a   id="link_tag" target="_blank" wire:dblclick="lol" class="articles__link">
         <div class="articles__content articles__content--lhs">
             <div class="articles__toper mb-1">
                 <button wire:click="component_edit_text_cheng">lol</button>
-                <p  class="ml-1">lol1</p>
+                <p  class="ml-1">{{$textt}}</p>
             </div>
             <x-dynamic-component :component="$component_edit_text" class="articles__title" text="Чебурек не человек, человек не чебурек"   />
           <div class="articles__footer">
             <p>Cakes</p>
           </div>
         </div>
-        <a  href="http://htmlbook.ru/html/a" class="articles__link">
         <div class="articles__content articles__content--rhs" aria-hidden="true">
             <div class="articles__toper mb-1">
                 <p class="ml-1">lol</p>
@@ -22,5 +22,35 @@
           </div>
         </div></a>
     </li>
+
+    <script>
+        var pendingClick = 0;
+
+        function xorClick(e) {
+            if (pendingClick) {
+                clearTimeout(pendingClick);
+                pendingClick = 0;
+            }
+
+            switch (e.detail) {
+                case 1:
+                    pendingClick = setTimeout(() => {
+                        window.open("http://htmlbook.ru/html/a")
+                    }, 500);
+                    break;
+                case 2:
+                    console.log('liiil')
+                    break;
+
+                default:
+                    console.log('luuul')
+                    break;
+            }
+        }
+
+
+        link_tag.addEventListener('click', xorClick, false);
+
+    </script>
 
 
